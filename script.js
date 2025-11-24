1434fb1c72253de34bc59b47fa27b8eeb8a05f93
wxnvxa <wxnvxa@gmail.com>
2025-11-24 11:56:02 +0500

    Update script.js

--- script.js
+++ script.js
@@ -84,3 +84,31 @@ window.addEventListener('resize', () => {
 
 init();
 animate();
+
+// --- Логика Модального Окна (Store) ---
+
+const modal = document.getElementById('soonModal');
+const buyBtns = document.querySelectorAll('.buy-btn');
+const closeBtn = document.querySelector('.close-modal');
+
+if (modal && buyBtns.length > 0) {
+    // Открытие окна при клике на ЛЮБУЮ кнопку купить
+    buyBtns.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            e.preventDefault(); // Чтобы страница не прыгала
+            modal.classList.add('active');
+        });
+    });
+
+    // Закрытие по крестику
+    closeBtn.addEventListener('click', () => {
+        modal.classList.remove('active');
+    });
+
+    // Закрытие при клике вне окна
+    window.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            modal.classList.remove('active');
+        }
+    });
+}
